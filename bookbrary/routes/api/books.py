32c0e9f76b2a524058f80d7e8880ba7b9eacdc39
Derk Weijers <derk@weijers.xyz>
2024-02-02 10:02:23 +0100

    Add GET method to books route

--- bookbrary/routes/api/books.py
+++ bookbrary/routes/api/books.py
@@ -6,6 +6,6 @@ from bookbrary.schemas import books_schema
 
 books = Blueprint(name="books", import_name=__name__, url_prefix="/api/books")
 
-@books.route(rule="/")
+@books.route(rule="/", methods=["GET"])
 def index():
     return books_schema.dump(obj=Book.query.all())
