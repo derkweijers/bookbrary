06e5f2a3259e3d2cad97bb3dbd7dd313a69d7f8c
Derk Weijers <derk@weijers.xyz>
2024-02-03 12:07:24 +0100

    Add database initialization in test fixture

--- bookbrary/tests/conftest.py
+++ bookbrary/tests/conftest.py
@@ -3,6 +3,7 @@ from flask import Flask
 import pytest
 from bookbrary.app import create_app
 from bookbrary.services import user_service
+from bookbrary.deps import db
 
 
 @pytest.fixture
@@ -10,8 +11,10 @@ def app() -> Generator[Flask, Any, None]:
     app = create_app()
     app.config.update({
         "TESTING": True,
-        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
     })
+
+    with app.app_context():
+        db.create_all()
 
     yield app
 
