24411e8c9744ee872b5bedbbba60a1cff07f1f20
Derk Weijers <derk@weijers.xyz>
2024-02-08 16:56:48 +0100

    Refactor create_user test in test_cli.py

--- bookbrary/tests/test_cli.py
+++ bookbrary/tests/test_cli.py
@@ -2,8 +2,8 @@ from flask.testing import FlaskCliRunner
 from bookbrary.cli import create_user
 
 
-def test_create_user(runner: FlaskCliRunner):
-    result = runner.invoke(create_user, ["testuser", "testpassword"])
+def test_create_user(runner: FlaskCliRunner) -> None:
+    result = runner.invoke(cli=create_user, args=["testuser", "testpassword"])
 
     assert result.exit_code == 0
     assert "User testuser created successfully." in result.output
