ce220e79c2795c820e48f126b18f3e9679cc4743
Derk Weijers <derk@weijers.xyz>
2024-02-09 08:03:51 +0100

    Refactor test cases and add test class structure

--- bookbrary/tests/test_books.py
+++ bookbrary/tests/test_books.py
@@ -5,70 +5,74 @@ from bookbrary.models.books import Book
 from bookbrary.models.users import User
 
 
-def test_get_all_books(client: FlaskClient) -> None:
-    response = client.get("/api/books/")
-    assert response.status_code == 200
+class TestBooks:
+    def test_get_all_books(self, client: FlaskClient) -> None:
+        response = client.get("/api/books/")
+        assert response.status_code == 200
 
-
-@pytest.mark.parametrize(
-    "test_title,test_created_at, expected",
-    [
-        ("Test Book 1", "2021-01-01", 201),
-        ("Test Book 2", True, 400),
-        ("Test Book", "2021-01-01", 409),
-    ],
-)
-def test_create_book(
-    client: FlaskClient, user: User, book: Book, test_title, test_created_at, expected
-) -> None:
-    auth_response = client.post(
-        "/api/auth/login",
-        json={
-            "username": "testuser",
-            "password": "validpassword",
-        },
+    @pytest.mark.parametrize(
+        "test_title,test_created_at, expected",
+        [
+            ("Test Book 1", "2021-01-01", 201),
+            ("Test Book 2", True, 400),
+            ("Test Book", "2021-01-01", 409),
+        ],
     )
-    assert auth_response.status_code == 200
-    assert auth_response.json
-    assert auth_response.json["access_token"]
+    def test_create_book(
+        self,
+        client: FlaskClient,
+        user: User,
+        book: Book,
+        test_title,
+        test_created_at,
+        expected,
+    ) -> None:
+        auth_response = client.post(
+            "/api/auth/login",
+            json={
+                "username": "testuser",
+                "password": "validpassword",
+            },
+        )
+        assert auth_response.status_code == 200
+        assert auth_response.json
+        assert auth_response.json["access_token"]
 
-    response = client.post(
-        "/api/books/",
-        headers={"Authorization": f"Bearer {auth_response.json['access_token']}"},
-        json={
-            "title": test_title,
-            "author": "Test Author",
-            "year": "2021",
-            "genre": "Test Genre",
-            "created_at": test_created_at,
-            "updated_at": "2021-01-01",
-        },
-    )
-    assert response.status_code == expected
+        response = client.post(
+            "/api/books/",
+            headers={"Authorization": f"Bearer {auth_response.json['access_token']}"},
+            json={
+                "title": test_title,
+                "author": "Test Author",
+                "year": "2021",
+                "genre": "Test Genre",
+                "created_at": test_created_at,
+                "updated_at": "2021-01-01",
+            },
+        )
+        assert response.status_code == expected
 
+    def test_no_data(self, client: FlaskClient, user: User) -> None:
+        auth_response = client.post(
+            "/api/auth/login",
+            json={
+                "username": "testuser",
+                "password": "validpassword",
+            },
+        )
+        assert auth_response.status_code == 200
+        assert auth_response.json
+        assert auth_response.json["access_token"]
 
-def test_no_data(client: FlaskClient, user: User) -> None:
-    auth_response = client.post(
-        "/api/auth/login",
-        json={
-            "username": "testuser",
-            "password": "validpassword",
-        },
-    )
-    assert auth_response.status_code == 200
-    assert auth_response.json
-    assert auth_response.json["access_token"]
+        response = client.post(
+            "/api/books/",
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {auth_response.json['access_token']}",
+            },
+        )
+        assert response.status_code == 400
 
-    response = client.post(
-        "/api/books/",
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {auth_response.json['access_token']}",
-        },
-    )
-    assert response.status_code == 400
-
-
-def test_get_single_book(client: FlaskClient, book: Book) -> None:
-    response = client.get(f"/api/books/{book.id}")
-    assert response.status_code == 200
+    def test_get_single_book(self, client: FlaskClient, book: Book) -> None:
+        response = client.get(f"/api/books/{book.id}")
+        assert response.status_code == 200
